60e8452e5816f92d21a87a3103864b14f9b54626
laske43 <61785371+laske43@users.noreply.github.com>
2020-05-24 10:27:55 +0200

    dodat header za goste

---
+++ 6. Realizacija/projekat/app/Views/sabloni/gostHeader.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Library</title>
+    <link rel="icon" href="<?php echo base_url(); ?>/assets/images/icon.jpg"/>  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel='stylesheet' href='<?php echo base_url(); ?>/assets/css/style.css'>
+</head>
+<body class="texture">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <!-- logo -->
+        <a class="navbar-brand" href="<?php echo site_url($controller) ?>"><img src="<?php echo base_url(); ?>/assets/images/logo.jpg" alt="LOGO" class="img-fluid"></a>
+        
+        
+        
+       
+        <ul class="navbar-nav" name="gost">
+            <?php
+                if($akcija=="prijava") {
+                echo '<li class="nav-item ';
+                echo '">
+                    <a class="nav-link" href="'.site_url($controller."/registrujse").'">Registruj Se</a>
+                </li>';
+                }
+                if ($akcija=='registracija') {
+                  echo '<li class="nav-item ';
+                echo '">
+                    <a class="nav-link" href="'.site_url($controller."/prijavise").'">Prijavi Se</a>
+                </li>';  
+                }
+              ?>
+        </ul>
+    </nav>
